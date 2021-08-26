1f68534f6a9b29963063d883bdffdf27dac9b92f
sirakisaias <sirakisaias@gmail.com>
2021-08-25 18:48:07 -0700

    working

--- api/server.js
+++ api/server.js
@@ -15,5 +15,12 @@ const authRouter = require('./auth/auth_router')
 server.use('/api/users', usersRouter)
 server.use('/api/auth', authRouter)
 
+server.get('/', (req, res) => {
+  res.json(`Welcome to the TT-33 Anywhere Fitness server! 🚴‍♀️`)
+})
+server.get('/api', (req, res) => {
+  res.json(`The TT-33 Anywhere Fitness api! 🚴‍♀🏃‍♂️ Try accessing this endpoint /classes`)
+})
+
 
 module.exports = server
